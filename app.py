dbac732f77f4506d30d7e4c497719a22f9d69405
Axel Simms <mail@axelsimms.com>
2020-06-23 11:41:33 -0400

    added docstrings to code written so far, this also removes linting warnings

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+"""The REST API backend for the ouchcounter, a chronic pain logger
+"""
 import os
 import datetime
 from flask import Flask, request, jsonify
@@ -27,7 +29,11 @@ ma = Marshmallow(app)
 
 # Create a Pain Object
 class PainEntry(db.Model):
+    """The database model for a single pain entry
 
+    Args:
+        db (Model): takes in a database model
+    """
     id = db.Column(db.Integer, primary_key=True)
     pain_level = db.Column(db.Integer)
     date = db.Column(db.DateTime)
@@ -38,7 +44,15 @@ class PainEntry(db.Model):
 
 # Create a schema with marshmallow
 class PainEntrySchema(ma.Schema):
+    """[summary]
+
+    Args:
+        ma (Marshmellow): a schema wrapper (???)
+        learn more about marshmellow and how that works
+    """
     class Meta:
+        """holds the fields for the schema
+        """
         fields = ('pain_level', 'date')
 
 # Init schema
@@ -52,11 +66,21 @@ pain_entries_schema = PainEntrySchema(many=True)
 # Home route
 @app.route('/', methods=['GET'])
 def homepage():
+    """route for the home url, confirms site is working
+
+    Returns:
+        [str]: a greeting message
+    """
     return "Hello world"
 
 # Add a pain entry
 @app.route('/new-ouchie', methods=['PUT'])
 def add_pain_entry():
+    """adds a pain entry via JSON to the log
+
+    Returns:
+        pain_entry (json) : returns a json object formatted by the pain entry data schema
+    """
     pain_level = request.json['pain_level']
     date = datetime.datetime.now()
 
@@ -70,6 +94,14 @@ def add_pain_entry():
 # Add a pain entry: but more automated
 @app.route('/new-ouchie/<painLevel>', methods=['PUT'])
 def add_pain_entry_url(pain_level):
+    """adds a pain entry to the log via URL arguments
+
+    Args:
+        pain_level (int): level of pain as an int between 1 and 10
+
+    Returns:
+        pain_entry (json) : returns a json object formatted by the pain entry data schema
+    """
     date = datetime.datetime.now()
 
     new_pain_entry = PainEntry(pain_level, date)
@@ -82,6 +114,11 @@ def add_pain_entry_url(pain_level):
 # Return all pain logs
 @app.route('/all-ouchies', methods=['GET'])
 def show_pain_entries():
+    """returns all of the pain entries from the database
+
+    Returns:
+        (json) : an array of all pain entry objects
+    """
     all_pain_entries = PainEntry.query.all()
     result = pain_entries_schema.dump(all_pain_entries)
     return jsonify(result)
